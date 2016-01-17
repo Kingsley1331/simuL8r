1ca1184d3d9f3b4e2b41b7ecf45202b67bbd1111
Kingsley1331 <kingankomah@hotmail.co.uk>
2016-01-17 18:33:23 +0000

    made it possible to dynamically choose between local and remote databases

--- database.js
+++ database.js
@@ -1,6 +1,15 @@
 var databaseName = 'simulations';
+var localDbUrl = 'mongodb://localhost/' + databaseName
+var remoteDbUrl = 'mongodb://heroku_dcp40x7c:v1c5jdm556bn1ocpfh6tqlo2mo@ds047355.mongolab.com:47355/heroku_dcp40x7c';
 
+var dbUrl = process.env.PORT ? remoteDbUrl : localDbUrl;
+
+/*
 module.exports = {
 	//'url' : 'mongodb://localhost/' + databaseName
 	'url' : 'mongodb://heroku_dcp40x7c:v1c5jdm556bn1ocpfh6tqlo2mo@ds047355.mongolab.com:47355/heroku_dcp40x7c'
+};*/
+
+module.exports = {
+	'url' : dbUrl
 };
