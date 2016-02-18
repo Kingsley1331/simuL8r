199d5bd1fb16c72f71913a144ee42de19627d46e
Kingsley1331 <kingankomah@hotmail.co.uk>
2016-02-18 23:59:13 +0000

    changed Google sign in settings

--- config/auth.js
+++ config/auth.js
@@ -1,4 +1,4 @@
-module.exports = {
+/*module.exports = {
 	'facebookAuth' : {
 		'clientID' : '159678581032017',
 		'clientSecret' : '30aa4ca491f6ac65d2940387d94a8b49',
@@ -7,7 +7,20 @@ module.exports = {
 	'googleAuth' : {
 		'clientID' : '375596617957-qjsj0br8lrh7ms5pq1qh5816vjgitphp.apps.googleusercontent.com',
 		'clientSecret' : 'Xn27GAxuBem2r5ZcyUgOi2Zo',
-		'callbackURL' : 'http://localhost:3000/auth/google/callback'
+		'callbackURL' : 'http://localhost:5000/auth/google/callback'
+	}
+}*/
+
+module.exports = {
+	'facebookAuth' : {
+		'clientID' : '159678581032017',
+		'clientSecret' : '30aa4ca491f6ac65d2940387d94a8b49',
+		'callbackURL' : 'http://localhost:3000/auth/facebook/callback'
+	},
+	'googleAuth' : {
+		'clientID' : '375596617957-d3n2uclct4bt64c5qij2vcv0lbq81qic.apps.googleusercontent.com',
+		'clientSecret' : 'Gm15Q7jBUr1nlPwyazQtRaK8',
+		'callbackURL' : 'https://intense-atoll-8693.herokuapp.com/auth2/callback'
 	}
 }
 
