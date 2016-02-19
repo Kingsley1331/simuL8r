29910bbd22e061d82c5018df91245d310086f24e
Kingsley1331 <kingankomah@hotmail.co.uk>
2016-02-19 00:23:27 +0000

    corrected typo in redirect URI

--- config/auth.js
+++ config/auth.js
@@ -15,12 +15,14 @@ module.exports = {
 	'facebookAuth' : {
 		'clientID' : '159678581032017',
 		'clientSecret' : '30aa4ca491f6ac65d2940387d94a8b49',
+		//'callbackURL' : 'http://localhost:3000/auth/facebook/callback'
 		'callbackURL' : 'http://localhost:3000/auth/facebook/callback'
 	},
 	'googleAuth' : {
 		'clientID' : '375596617957-d3n2uclct4bt64c5qij2vcv0lbq81qic.apps.googleusercontent.com',
 		'clientSecret' : 'Gm15Q7jBUr1nlPwyazQtRaK8',
-		'callbackURL' : 'https://intense-atoll-8693.herokuapp.com/auth2/callback'
+		'callbackURL' : 'https://intense-atoll-8693.herokuapp.com/oauth2/callback'
+		//'callbackURL' : 'http://localhost:5000/auth2/callback'
 	}
 }
 
